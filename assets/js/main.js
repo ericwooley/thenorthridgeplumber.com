e4b006a3153f9331eae1c97f134c190a1c13ddbd
Eric Wooley <ericwooley@gmail.com>
2015-03-22 16:37:33 -0700

    Added slick and some basics

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slick').slick({
+        infinite: true,
+        variableWidth: true,
+        speed: 2000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+                
+});
---
+++ pages/plumbing_services.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: Plumbing Services
+permalink: /plumbingservices/
+---
+
+Have some plumbing services

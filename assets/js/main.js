26d6f9989e7be16eb851884befdfc968dade34b8
Eric Wooley <ericwooley@gmail.com>
2015-03-22 17:04:43 -0700

    fixed screen width issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 $(function(){
+    $('.slick').show();
     $('.slick').slick({
         infinite: true,
         variableWidth: true,
         speed: 2000,
         autoplay: true,
         autoplaySpeed: 1000,
+        arrows: false
     });
                 
 });

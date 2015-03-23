015af168a46176527abd2712a303649ec56941db
Eric Wooley <ericwooley@gmail.com>
2015-03-22 21:14:50 -0700

    added bootstrap themes and made nav an phone work on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 $(function(){
-    $('.site-header').sticky();   
+    $('.site-header').sticky();
+    var waypoints = $('.way-fade');
+    _.each(waypoints, function(waypoint){
+        new Waypoint({
+            element: waypoint,
+            offset: '18%',
+            handler: function(direction) {
+                $(this.element).animate({
+                    opacity: direction === 'up'? 1: 0,
+                }, 'slow');
+            }
+        });
+        new Waypoint({
+            element: waypoint,
+            offset: '85%',
+            handler: function(direction) {
+                $(this.element).animate({
+                    opacity: direction === 'up'? 0: 1,
+                }, 'slow');
+            }
+        });
+    });
 });

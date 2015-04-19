d3fced93bd9b8923e6ce8069b153a7dbbd616d59
Eric Wooley <ericwooley@gmail.com>
2015-04-19 15:58:20 -0700

    workking on square in affect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,36 @@
+
+/* globals $, Waypoint, _ */
 $(function(){
+    'use strict';
     $('.site-header').sticky();
     var waypoints = $('.way-fade');
     _.each(waypoints, function(waypoint){
         new Waypoint({
             element: waypoint,
-            offset: '10%',
+            offset: '60%',
             handler: function(direction) {
-                $(this.element).velocity({
-                    opacity: direction === 'up'? 1: 0,
-                }, 'fast');
+                if(direction === 'down'){
+                    $(this.element).addClass('show-badge');
+                }
             }
         });
-        new Waypoint({
-            element: waypoint,
-            offset: '85%',
-            handler: function(direction) {
-                $(this.element).animate({
-                    opacity: direction === 'up'? 0: 1,
-                }, 'slow');
-            }
-        });
+        // new Waypoint({
+        //     element: waypoint,
+        //     offset: '10%',
+        //     handler: function(direction) {
+        //         $(this.element).velocity({
+        //             opacity: direction === 'up'? 1: 0,
+        //         }, 'fast');
+        //     }
+        // });
+        // new Waypoint({
+        //     element: waypoint,
+        //     offset: '85%',
+        //     handler: function(direction) {
+        //         $(this.element).animate({
+        //             opacity: direction === 'up'? 0: 1,
+        //         }, 'slow');
+        //     }
+        // });
     });
 });

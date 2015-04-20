ad338375d5dbbc4f8f96d95facf550298cf5057f
Eric Wooley <ericwooley@gmail.com>
2015-04-19 18:29:55 -0700

    fixed side bars

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,23 +14,5 @@ $(function(){
                 }
             }
         });
-        // new Waypoint({
-        //     element: waypoint,
-        //     offset: '10%',
-        //     handler: function(direction) {
-        //         $(this.element).velocity({
-        //             opacity: direction === 'up'? 1: 0,
-        //         }, 'fast');
-        //     }
-        // });
-        // new Waypoint({
-        //     element: waypoint,
-        //     offset: '85%',
-        //     handler: function(direction) {
-        //         $(this.element).animate({
-        //             opacity: direction === 'up'? 0: 1,
-        //         }, 'slow');
-        //     }
-        // });
     });
 });
